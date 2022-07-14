2f9f1e5a7e3fcebed5777073b09f604a9a1ad3db
StMacoli <st.macoli@gmail.com>
2022-07-14 11:30:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CardList from "./CardList/CardList";
+
+function App() {
+  return (
+    <div className="App">
+      <CardList />     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CardList/CardList.js
@@ -0,0 +1,11 @@
+const CardList = () => {
+    return (
+        <h1>Hello world</h1>
+    )
+}
+
+//function CardList () {
+
+//}
+
+export default CardList;
